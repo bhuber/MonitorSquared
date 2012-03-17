37045a2f0bf0d59b5384ce6e4d879a50d6a2e49b
Bennet Huber <bhuber@azavea.com>
2012-03-16 23:36:20 -0400

    Updated docs and install script, added .gitignore

--- scripts/install/installEverything.sh
+++ scripts/install/installEverything.sh
@@ -8,8 +8,19 @@ if [[ $(whoami) != "root" ]]; then
 	exit 1;
 fi
 
+PROJECT_ROOT="../.."
+USERNAME="YOUR_LOCAL_USERNAME"
+
+echo "Make sure to set the USERNAME variable to the correct value before running this script (current value is $USERNAME)"
+echo "Once you have, comment out the exit command after this echo message"
+exit
+
 #apt-get install python-django
-apt-get install python-pip
+apt-get install python2.7 python-setuptools python-pip
 apt-get install postgresql
 pip install virtualenv
+
+cd $PROJECT_ROOT/py
+su -c "virtualenv --no-site-packages venv" $USERNAME
+su -c "./venv/bin/easy_install django" $USERNAME
 
