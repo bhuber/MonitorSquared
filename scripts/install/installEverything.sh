939c940b8bb26c7826e54bb3427e98f9d5d0f8d4
Bennet Huber <bhuber@azavea.com>
2012-03-16 23:04:08 -0400

    Added dependency install script

---
+++ scripts/install/installEverything.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script produces a ton of output, so you should probably call it as follows:
+# ./installEverything.sh 2>&1 | tee installEverything.log
+
+if [[ $(whoami) != "root" ]]; then
+	echo "You must run this script as root or it won't work"
+	exit 1;
+fi
+
+apt-get install python-django, python-pip, postgresql
+pip install virtualenv
+
